0d600fc732be1ad7937f1b3a5b1df1b3486bb813
mohammad jamshidia <mohammad_jamshida@yahoo.com>
2024-02-21 12:11:41 +0330

    set Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import ProductsPage from "./pages/ProductsPage"
+import CheckOutPage from "./pages/CheckOutPage"
+import PageNotFound from "./pages/PageNotFound"
+import DetailsPage from "./pages/DetailsPage"
 
 
 function App() {
@@ -5,7 +10,13 @@ function App() {
 
   return (
     <>
-      Shop
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/CheckOutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckOutPage() {
+  return (
+    <div>
+      CheckOutPage
+    </div>
+  )
+}
+
+export default CheckOutPage
