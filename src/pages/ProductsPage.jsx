d6453a0274602b3f5268d545fedd205a56453086
mohammad jamshidia <mohammad_jamshida@yahoo.com>
2024-03-12 17:47:52 +0330

    Rendering Data

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { useProducts } from '../context/ProductContext'
+import React from "react";
+import { useProducts } from "../context/ProductContext";
+import styles from "./ProductsPage.module.css";
 function ProductsPage() {
   const products = useProducts();
-  console.log(products)
+  console.log(products);
 
   return (
-    <div>
-      ProductsPage
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading ...</p>}
+        {products.map((p) => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>SideBar</div>
     </div>
-  )
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
