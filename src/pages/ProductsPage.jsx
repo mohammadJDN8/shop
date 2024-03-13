ea162a07b3b2310345a48d2b15e3bf6049d44ca4
mohammad jamshidia <mohammad_jamshida@yahoo.com>
2024-03-13 18:52:14 +0330

    Search Bar

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
+import { BiSearchAlt2 } from "react-icons/bi";
+
 function ProductsPage() {
   const products = useProducts();
-
+  const [search, setSearch] = useState("");
+  const searchHandler = () => {
+    console.log("search ");
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <BiSearchAlt2 />
+        </button>
       </div>
-      <div>SideBar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>SideBar</div>
+      </div>
+    </>
   );
 }
 
