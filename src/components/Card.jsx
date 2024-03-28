7b952d00c5aaf01e265b6002a370b3c0aa057a46
Mohammad jamshidnia <mohammad_jamshidnia@yahoo.com>
2024-03-29 00:18:43 +0330

    Button For Actions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,13 @@ import { IoBagCheckOutline } from "react-icons/io5";
 import { shortenText } from "../helper/Helper";
 import styles from "./Card.module.css";
 import { useCart } from "../context/CartContext";
+import { MdDeleteOutline } from "react-icons/md";
 
 function Card({ data }) {
   const [state, dispatch] = useCart();
   console.log(state);
-  const clickHandler = () => {
-    dispatch({ type: "ADD_ITEM", payload: data });
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
   };
   const { id, image, title, price } = data;
   return (
@@ -23,9 +24,14 @@ function Card({ data }) {
           <CgDetailsMore />
         </Link>
         <div>
-          <button onClick={clickHandler}>
+          <button onClick={() => clickHandler("ADD_ITEM")}>
             <IoBagCheckOutline />
           </button>
+          <button onClick={() => clickHandler("REMOVE_ITEM")}>
+            <MdDeleteOutline />
+          </button>
+          <button onClick={() => clickHandler("INCREASE")}>+</button>
+          <button onClick={() => clickHandler("DECREASE")}>-</button>
         </div>
       </div>
     </div>
